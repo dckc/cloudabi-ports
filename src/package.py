583b758d806bcd67bff7391b35bcc0987fcfe9f1
Dan Connolly <dckc@madmode.com>
2016-09-17 14:07:20 -0500

    src.package: static types, explicit authority

--- src/package.py
+++ src/package.py
@@ -3,23 +3,28 @@
 # This file is distributed under a 2-clause BSD license.
 # See the LICENSE file for details.
 
+from typing import Callable, Optional, Set, cast
 import logging
-import os
-import shutil
-import stat
-import subprocess
 
 from . import config
 from . import util
 from .builder import BuildDirectory, BuildHandle, HostBuilder, TargetBuilder
+from .util import PathExt
 
 log = logging.getLogger(__name__)
 
+WHAT = int
+
+
 class HostPackage:
 
-    def __init__(self, install_directory, name, version, homepage,
-                 maintainer, build_depends, lib_depends, distfiles,
-                 build_cmd, resource_directory):
+    def __init__(self, install_directory: PathExt,
+                 name: str, version: str, homepage :str, maintainer: str,
+                 build_depends: Set[HostPackage],
+                 lib_depends: Set[HostPackage],
+                 distfiles: WHAT,
+                 build_cmd: Callable[[BuildHandle], None],
+                 resource_directory: PathExt) -> None:
         self._install_directory = install_directory
         self._name = name
         self._version = version
@@ -27,32 +32,34 @@ class HostPackage:
         self._build_cmd = build_cmd
         self._resource_directory = resource_directory
 
+        self._build_depends = set()  # type: Set[HostPackage]
+        self._lib_depends = set()    # type: Set[HostPackage]
+
         # Compute the set of transitive build dependencies.
-        self._build_depends = set()
         for dep in build_depends:
             self._build_depends.add(dep)
             self._build_depends |= dep._lib_depends
 
         # Compute the set of transitive library dependencies.
-        self._lib_depends = set()
         for dep in lib_depends:
             self._lib_depends.add(dep)
             self._lib_depends |= dep._lib_depends
 
-    def _initialize_buildroot(self):
+    def _initialize_buildroot(self) -> None:
         # Ensure that all dependencies have been built.
         deps = self._build_depends | self._lib_depends
         for dep in deps:
             dep.build()
 
         # Install dependencies into an empty buildroot.
-        util.remove_and_make_dir(config.DIR_BUILDROOT)
+        allfiles = self._install_directory  # KLUDGE
+        util.remove_and_make_dir(allfiles / config.DIR_BUILDROOT)
         for dep in deps:
             dep.extract()
 
-    def build(self):
+    def build(self) -> None:
         # Skip this package if it has been built already.
-        if os.path.isdir(self._install_directory):
+        if self._install_directory.is_dir():
             return
 
         # Perform the build inside an empty buildroot.
@@ -66,17 +73,22 @@ class HostPackage:
 
     def extract(self):
         # Copy files literally.
+        allfiles = self._install_directory  # KLUDGE
         for source_file, target_file in util.walk_files_concurrently(
-                self._install_directory, config.DIR_BUILDROOT):
+                self._install_directory, allfiles / config.DIR_BUILDROOT):
             util.make_parent_dir(target_file)
             util.copy_file(source_file, target_file, False)
 
 
 class TargetPackage:
 
-    def __init__(self, install_directory, arch, name, version, homepage,
-                 maintainer, host_packages, lib_depends, build_cmd,
-                 distfiles, resource_directory):
+    def __init__(self, install_directory: PathExt,
+                 arch: str, name: str, version: str, homepage: str,
+                 maintainer: str,
+                 host_packages: Dict[str, HostPackage],
+                 lib_depends: Set[TargetPackage],
+                 build_cmd: Optional[Callable[[BuildHandle], None]],
+                 distfiles: WHAT, resource_directory: PathExt) -> None:
         self._install_directory = install_directory
         self._arch = arch
         self._name = name
@@ -89,7 +101,7 @@ class TargetPackage:
         self._resource_directory = resource_directory
 
         # Compute the set of transitive library dependencies.
-        self._lib_depends = set()
+        self._lib_depends = set()  # type: Set[TargetPackage]
         for dep in lib_depends:
             if dep._build_cmd:
                 self._lib_depends.add(dep)
@@ -100,7 +112,7 @@ class TargetPackage:
 
     def build(self):
         # Skip this package if it has been built already.
-        if not self._build_cmd or os.path.isdir(self._install_directory):
+        if not self._build_cmd or self._install_directory.is_dir():
             return
 
         # Perform the build inside a buildroot with its dependencies
@@ -126,15 +138,15 @@ class TargetPackage:
         for source_file, target_file in util.walk_files_concurrently(
                 self._install_directory, path):
             util.make_parent_dir(target_file)
-            if target_file.endswith('.template'):
+            if target_file.suffix == '.template':
                 # File is a template. Expand %%PREFIX%% tags.
-                target_file = target_file[:-9]
-                with open(source_file, 'r') as f:
+                target_file = target_file.with_name(target_file.name[:-9])
+                with source_file.open(mode='r') as f:
                     contents = f.read()
                 contents = contents.replace('%%PREFIX%%', expandpath)
-                with open(target_file, 'w') as f:
+                with target_file.open(mode='w') as f:
                     f.write(contents)
-                shutil.copymode(source_file, target_file)
+                source_file.copymode(target_file)
             else:
                 # Regular file. Copy it over literally.
                 util.copy_file(source_file, target_file, False)
@@ -169,7 +181,7 @@ class TargetPackage:
     def get_homepage(self):
         return self._homepage
 
-    def get_lib_depends(self):
+    def get_lib_depends(self) -> Set[TargetPackage]:
         return self._lib_depends
 
     def get_maintainer(self):
@@ -181,23 +193,25 @@ class TargetPackage:
     def get_version(self):
         return self._version
 
-    def initialize_buildroot(self, host_depends, lib_depends=set()):
+    def initialize_buildroot(self, host_depends: List[str],
+                             lib_depends: Set[TargetPackage]=set()) -> None:
         # Ensure that all dependencies have been built.
         host_deps = set()
-        for dep in host_depends:
-            package = self._host_packages[dep]
+        for dep_name in host_depends:
+            package = self._host_packages[dep_name]
             host_deps.add(package)
             for depdep in package._lib_depends:
                 host_deps.add(depdep)
         for dep in host_deps:
             dep.build()
-        for dep in lib_depends:
-            dep.build()
+        for ldep in lib_depends:
+            ldep.build()
 
         # Install dependencies into an empty buildroot.
-        util.remove_and_make_dir(config.DIR_BUILDROOT)
+        allfiles = self._install_directory  # KLUDGE
+        util.remove_and_make_dir(allfiles / config.DIR_BUILDROOT)
         for dep in host_deps:
             dep.extract()
-        prefix = os.path.join(config.DIR_BUILDROOT, self._arch)
-        for dep in lib_depends:
-            dep.extract(prefix, prefix)
+        prefix = allfiles / config.DIR_BUILDROOT, self._arch
+        for ldep in lib_depends:
+            ldep.extract(prefix, prefix)
